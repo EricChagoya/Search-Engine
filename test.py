3f32949875e434e7cecfbb98f25a2ea1869c7dcb
Eric Chagoya <57156331+EricChagoya@users.noreply.github.com>
2020-05-01 19:38:01 -0700

    Add files via upload

--- test.py
+++ test.py
@@ -1,13 +1,6 @@
+import json
 
+with open('0a0056fb9a53ec6f190aa2b5fb1a97c33cd69726c8841f89d24fa5abd84d276c.json', 'r') as f:
+    array = json.load(f)
 
-
-
-
-
-
-
-print("Test")
-
-
-
-
+print (array)
