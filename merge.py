48df5d060718da2f29674d1b37c07b0f06221b42
Eric Chagoya <57156331+EricChagoya@users.noreply.github.com>
2020-05-21 21:32:06 -0700

    Add files via upload

--- merge.py
+++ merge.py
@@ -4,17 +4,19 @@ from LL import Postings
 
 
 
-
 def merge_index() -> None:
-    """Deal with file managment"""
+    """Deal with file managment. Input how many files you want to take in.
+    It will always output 5 files."""
     with open("output_indexer0.txt", "r", encoding = 'utf8') as f0, \
          open("output_indexer1.txt", "r", encoding = 'utf8') as f1, \
+         open("output_indexer2.txt", "r", encoding = 'utf8') as f2, \
+         open("output_indexer3.txt", "r", encoding = 'utf8') as f3, \
          open("0-9_output_indexer.txt", "w", encoding = 'utf8') as w0, \
          open("A-F_output_indexer.txt", "w", encoding = 'utf8') as w1, \
          open("G-M_output_indexer.txt", "w", encoding = 'utf8') as w2, \
          open("N-S_output_indexer.txt", "w", encoding = 'utf8') as w3, \
          open("T-Z_output_indexer.txt", "w", encoding = 'utf8') as w4:
-        files= [f0, f1]
+        files= [f0, f1, f2, 3]
         write= [w0, w1, w2, w3, w4]
         all_buffers= [add_buffer(f) for f in files]
         merging(all_buffers, write)
@@ -23,12 +25,11 @@ def merge_index() -> None:
 def add_buffer(open_file: 'file_object') -> [('token', 'Posting')]:
     """Load the next n lines into a file. Then convert them into postings"""
     count= 0
-    amount= 500       # The bigger the number, the faster it is
     total= []
     for line in open_file:
         total.append(line)
         count+= 1
-        if count >= amount:
+        if count >= 500:    # Size of the buffer
             yield create_reading_buffer(total)
             total= []
             count= 0
@@ -44,7 +45,6 @@ def create_reading_buffer(total:['str']) -> [('token', 'Posting')]:
         post= add_posting(line)
         buffer.append((line[0].lower(), post))
     return buffer
-
 
 
 def add_posting(line:[str]) -> "Postings":
@@ -63,15 +63,14 @@ def merging(r_files: ['generator'], w_files:['file_object']) -> None:
     indexes in alphabetical order. This way all the "a" indexes are in the
     same file"""
     tokens_post= [next(line) for line in r_files]
-    letters= ["0-9", "a-f", "g-m", "n-s", "t-z", "{"]
-    buffer_writing= []
     # [[('token', 'Posting')]] with each list representing a file
     #[[('0', Postings), ('0..100.', Postings)],
     # [('0', Postings), ('0.01', Postings)] ]
+    letters= ["0-9", "a-f", "g-m", "n-s", "t-z", "{"]
+    buffer_writing= []
     count= 0
     n= 0
     while len(r_files) > 0:
-            
         word, similar_index= same_word([file[0][0] for file in tokens_post])
         if word == "{":
             break
@@ -88,12 +87,27 @@ def merging(r_files: ['generator'], w_files:['file_object']) -> None:
         buffer_writing.append((word, post_line))
         count+= 1
         n+= 1
-        if count > 10000:
-            print(n)
+        
+        if count > 1000:
+            print(n)    # Checks merging progress
             write_postings(buffer_writing, w_files[0])
             count= 0
             buffer_writing= []
     write_postings(buffer_writing, w_files[0])
+
+
+def same_word(tokens:['str']) -> str and [int]:
+    """It sees what word appears first. If multiple files share
+    the same first word, then return a list of their indexes"""
+    first = "{"
+    similar_index = []
+    for n, token in enumerate(tokens):
+        if token == first:
+            similar_index.append(n)
+        elif token < first:
+            first= token
+            similar_index = [n]
+    return first, similar_index    
 
 
 def single_match(tokens_post:[[('tokens', 'Postings')]], r_files: ['file_objects'], n:int) -> 'Postings':
@@ -115,13 +129,12 @@ def single_match(tokens_post:[[('tokens', 'Postings')]], r_files: ['file_objects
 def multiple_match(tokens_post:[[('tokens', 'Postings')]], r_files: ['file_objects'], indexes:[int]) -> "Postings":
     n= indexes[0]
     post_line= tokens_post[n][0][1]
-    for i in range(1, len(indexes)):
+    for i in range(1, len(indexes)):    # Combines postings
         n= indexes[i]
         post_line.combine(tokens_post[n][0][1])
 
     for i in range(len(indexes) - 1, -1, -1):   # Solves the problem of removing indexing changes index
         n= indexes[i]
-            
         del tokens_post[n][0]
         if 1 > len(tokens_post[n]):
             next_batch= next(r_files[n])
@@ -133,67 +146,6 @@ def multiple_match(tokens_post:[[('tokens', 'Postings')]], r_files: ['file_objec
     return post_line
 
 
-
-def update_multiple(indexes:[int], r_files:['file_objects'], tokens:['str'],
-                    postings:['Posting']) -> 'Posting':
-    """It will return the posting. If the next line in the file is empty,
-    remove that index from the list of files, tokens, and postings. It does
-    this if multiple files share the same next word"""
-    n= indexes[0]
-    post_line= postings[n]
-
-    for i in range(1, len(indexes)):
-        n= indexes[i]
-        post_line.combine(postings[n])
-
-    for i in range(len(indexes) - 1, -1, -1):
-        n= indexes[i]
-        next_line= next(r_files[n])
-        if next_line == None:
-            del r_files[n]
-            del tokens[n]
-            del postings[n]
-        else:
-            tokens[n] = next_line[0]
-            postings[n]= next_line[1]
-    return post_line
-
-def alphabetical_indexer(r_files:['generator'], w_files:['file_object']) -> None:
-    """It iterates through previous indexes so it can create a couple of new
-    indexes in alphabetical order. This way all the "a" indexes are in the
-    same file"""
-    tokens, postings= starting_lines(r_files)    
-    letters= ["0-9", "a-f", "g-m", "n-s", "t-z"]
-    while len(r_files) > 0:
-        word, similar_index= same_word(tokens)
-        if len(similar_index) == 1:
-            post_line= update_single(similar_index[0], r_files, tokens, postings)
-        else:
-            post_line= update_multiple(similar_index, r_files, tokens, postings)
-
-        if word[0] > letters[0][-1]:
-            w_files.pop(0)
-            letters.pop(0)
-        write_single_posting(word, post_line, w_files[0])
-        
-
-
-
-
-def same_word(tokens:['str']) -> str and [int]:
-    """It sees what word appears first. If multiple files share
-    the same first word, then return a list of their indexes"""
-    first = "{"
-    similar_index = []
-    for n, token in enumerate(tokens):
-        if token == first:
-            similar_index.append(n)
-        elif token < first:
-            first= token
-            similar_index = [n]
-    return first, similar_index    
-
-
 def write_postings(buffer:[('token', 'Posting')], f:['file_object']) -> None:
     """Write the entire buffer into the given text file"""
     for line in buffer:
@@ -203,14 +155,6 @@ def write_postings(buffer:[('token', 'Posting')], f:['file_object']) -> None:
             f.write(" -> " + str(line[1].get_node()).strip() + "\t")
             line[1].next()
         f.write("\n")
-
-
-
-
-
-
-
-
 
 
 
@@ -260,10 +204,11 @@ def seek() -> None:
 if __name__ == '__main__':
     start= time.time()
     merge_index()  #Merging
-    
+
+    # Can't do this until you fix the merge list
     #seek()     # Find position of the letters in different files
     end= time.time()
-    print(end - start)
+    print("Time", (end - start)/60, "minutes")
 
 
 
